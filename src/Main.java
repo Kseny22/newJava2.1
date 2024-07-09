07faca40baf67fe5999cbdd7478898ed1ebdffbc
Emma Paris <misyura.c@yandex.ru>
2024-07-09 22:38:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int price = 19_700;
+        int bonus = price / 20;
+    System.out.println("Количество начисленных миль за купленный билет: " + bonus);
+
+  }
+}
